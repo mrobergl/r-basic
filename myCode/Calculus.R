b820134917580d9c11b9ee5b0f53f6b3eb02294f
Roberto Gainza <mrobergl@hotmail.com>
2020-01-04 20:07:40 +0100

    030120202007

---
+++ myCode/Calculus.R
@@ -0,0 +1,30 @@
+pi^2
+2*pi
+Inf
+-Inf
+NA
+NaN
+5/0
+0/0
+2^50
+sqrt(25)
+exp(1)
+log(exp(1))
+log(1000, 10)
+log10(10)
+abs(-pi)
+factorial(3)
+choose(3, 2)
+factorial(-5)
+6^log(4, 6)
+log(4^6,4)
+
+
+
+sin(pi/2)
+cos(pi/2)
+cos(pi)
+cos(3*pi/2)
+tan(0)
+sin(pi/2)
+asin(1) *180/pi
---
+++ myCode/InstallPackages.R
@@ -0,0 +1,2 @@
+install.packages("tidyverse", dependencies = TRUE)
+library("tidyverse")
